b76839990821193505cbdac2c49e89e4d50a0fe6
Tiffanie Lim <tiffanielim3147@gmail.com>
2026-09-21 17:27:28 -0700

    Build out grid UI

--- app/src/main/java/com/example/gridlayout/MainActivity.java
+++ app/src/main/java/com/example/gridlayout/MainActivity.java
@@ -3,141 +3,40 @@ package com.example.gridlayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.gridlayout.widget.GridLayout;
 
-import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
-import android.util.Log;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final int COLUMN_COUNT = 2;
-
-    // save the TextViews of all cells in an array, so later on,
-    // when a TextView is clicked, we know which cell it is
-    private ArrayList<TextView> cell_tvs;
-
-    private int dpToPixel(int dp) {
-        float density = Resources.getSystem().getDisplayMetrics().density;
-        return Math.round(dp * density);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        cell_tvs = new ArrayList<TextView>();
-
-        // Method (1): add statically created cells
-        TextView tv00 = (TextView) findViewById(R.id.textView00);
-        TextView tv01 = (TextView) findViewById(R.id.textView01);
-        TextView tv10 = (TextView) findViewById(R.id.textView10);
-        TextView tv11 = (TextView) findViewById(R.id.textView11);
-
-        tv00.setTextColor(Color.GRAY);
-        tv00.setBackgroundColor(Color.GRAY);
-        tv00.setOnClickListener(this::onClickTV);
-
-        tv01.setTextColor(Color.GRAY);
-        tv01.setBackgroundColor(Color.GRAY);
-        tv01.setOnClickListener(this::onClickTV);
-
-        tv10.setTextColor(Color.GRAY);
-        tv10.setBackgroundColor(Color.GRAY);
-        tv10.setOnClickListener(this::onClickTV);
-
-        tv11.setTextColor(Color.GRAY);
-        tv11.setBackgroundColor(Color.GRAY);
-        tv11.setOnClickListener(this::onClickTV);
-
-        cell_tvs.add(tv00);
-        cell_tvs.add(tv01);
-        cell_tvs.add(tv10);
-        cell_tvs.add(tv11);
-
-        // Method (2): add four dynamically created cells
-        GridLayout grid = (GridLayout) findViewById(R.id.gridLayout01);
-        for (int i = 2; i<=3; i++) {
-            for (int j=0; j<=1; j++) {
-                TextView tv = new TextView(this);
-                tv.setHeight( dpToPixel(64) );
-                tv.setWidth( dpToPixel(64) );
-                tv.setTextSize( 32 );//dpToPixel(32) );
-                tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
-                tv.setTextColor(Color.GRAY);
-                tv.setBackgroundColor(Color.GRAY);
-                tv.setOnClickListener(this::onClickTV);
-
-                GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
-                lp.setMargins(dpToPixel(2), dpToPixel(2), dpToPixel(2), dpToPixel(2));
-                lp.rowSpec = GridLayout.spec(i);
-                lp.columnSpec = GridLayout.spec(j);
-
-                grid.addView(tv, lp);
-
-                cell_tvs.add(tv);
-            }
-            initializeBoard();
-            logBoard();
-        }
-
-        // Method (3): add four dynamically created cells with LayoutInflater
-        LayoutInflater li = LayoutInflater.from(this);
-        for (int i = 4; i<=5; i++) {
-            for (int j=0; j<=1; j++) {
-                TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
-                //tv.setText(String.valueOf(i)+String.valueOf(j));
-                tv.setTextColor(Color.GRAY);
-                tv.setBackgroundColor(Color.GRAY);
-                tv.setOnClickListener(this::onClickTV);
-
-                GridLayout.LayoutParams lp = (GridLayout.LayoutParams) tv.getLayoutParams();
-                lp.rowSpec = GridLayout.spec(i);
-                lp.columnSpec = GridLayout.spec(j);
-
-                grid.addView(tv, lp);
-
-                cell_tvs.add(tv);
-            }
-        }
-
-    }
-
-    private int findIndexOfCellTextView(TextView tv) {
-        for (int n=0; n<cell_tvs.size(); n++) {
-            if (cell_tvs.get(n) == tv)
-                return n;
-        }
-        return -1;
-    }
-
-    public void onClickTV(View view){
-        TextView tv = (TextView) view;
-        int n = findIndexOfCellTextView(tv);
-        int i = n/COLUMN_COUNT;
-        int j = n%COLUMN_COUNT;
-        tv.setText(String.valueOf(i)+String.valueOf(j));
-        if (tv.getCurrentTextColor() == Color.GRAY) {
-            tv.setTextColor(Color.GREEN);
-            tv.setBackgroundColor(Color.parseColor("lime"));
-        }else {
-            tv.setTextColor(Color.GRAY);
-            tv.setBackgroundColor(Color.LTGRAY);
-        }
-    }
 
     private static final int GRID_SIZE = 10;
     private static final int MINE_COUNT = 6;
     private static final String TAG = "Minesweeper";
 
     private Cell[][] board = new Cell[GRID_SIZE][GRID_SIZE];
+    private TextView[][] cellViews = new TextView[GRID_SIZE][GRID_SIZE];
+
+    private TextView mineCountTv;
+    private TextView timerTv;
+    private TextView modeIconTv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mineCountTv = findViewById(R.id.mineCountTv);
+        timerTv = findViewById(R.id.timerTv);
+        modeIconTv = findViewById(R.id.modeIconTv);
+
+        initializeBoard();
+        logBoard();
+        buildGridUI();
+    }
 
     private void initializeBoard() {
         for (int r = 0; r < GRID_SIZE; r++)
@@ -181,6 +80,35 @@ public class MainActivity extends AppCompatActivity {
                 row.append(board[r][c].isMine ? "M" : board[r][c].adjacentMines).append(" ");
             }
             Log.d(TAG, row.toString());
+        }
+    }
+
+    // TEMPORARY: reveals everything so I can check grid lines up with the
+    // Logcat output. Will replace w real dig/flag logic
+    private void buildGridUI() {
+        GridLayout grid = findViewById(R.id.gridLayout01);
+        LayoutInflater li = LayoutInflater.from(this);
+
+        for (int r = 0; r < GRID_SIZE; r++) {
+            for (int c = 0; c < GRID_SIZE; c++) {
+                TextView tv = (TextView) li.inflate(R.layout.custom_cell_layout, grid, false);
+
+                Cell cell = board[r][c];
+                if (cell.isMine) {
+                    tv.setText("M");
+                    tv.setBackgroundColor(Color.parseColor("#FF6666"));
+                } else {
+                    tv.setText(String.valueOf(cell.adjacentMines));
+                    tv.setBackgroundColor(Color.LTGRAY);
+                }
+
+                GridLayout.LayoutParams lp = (GridLayout.LayoutParams) tv.getLayoutParams();
+                lp.rowSpec = GridLayout.spec(r);
+                lp.columnSpec = GridLayout.spec(c);
+
+                grid.addView(tv, lp);
+                cellViews[r][c] = tv;
+            }
         }
     }
 }
